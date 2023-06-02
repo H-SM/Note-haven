b786121053be37c3f7d5d31b6bade241e1349942
Harman Singh <98532264+H-SM@users.noreply.github.com>
2023-06-02 19:40:15 +0530

    Made changes over the Navbar, added a bootstrap form in Home

--- src/context/Notes/NoteState.js
+++ src/context/Notes/NoteState.js
@@ -1,21 +1,10 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import React from "react";
 import NoteContext from "./noteContext";
 const NoteState = (props) =>{
-    const s1 = {
-        "name" : "HSM",
-        "class": "B1 CSE"
-    }
-    const [ state , setState ] = useState(s1);
-    const update = () =>{
-        setTimeout(()=>{
-            setState({
-                "name" : "H-S-M",
-                "class" : "3rd year"
-            })
-        },2000);
-    }
+   
     return (
-    <NoteContext.Provider value={{state, update}}>
+    <NoteContext.Provider value={{}}>
         {props.children}
     </NoteContext.Provider>
     );
