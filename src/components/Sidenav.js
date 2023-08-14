321e6f891288497e1ccf33208babd799236c9bbd
Harman Singh <98532264+H-SM@users.noreply.github.com>
2023-08-14 18:04:38 +0530

    added a logo

--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -1,6 +1,6 @@
 // import React, {useEffect} from 'react';
 import React from 'react';
-
+import logo from '../assets/Logo.png';
 const Sidenav = () => {
 
   return (
@@ -9,6 +9,17 @@ const Sidenav = () => {
               id="sidenav-3"
               className="left-0 top-0 h-screen w-[300px] lg:w-[400px] overflow-hidden bg-zinc-800 shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)]">
               {/* Side Navbar content */}
+              <div className='flex flex-col h-full items-center '>
+              
+              <img src={logo} alt="logo"/>
+              <div className='flex flex-col justify-between h-full items-center px-6 py-6'>
+             
+              <h1 className='text-secondary-white text-[34px]'>tags</h1>
+              <h1 className='text-secondary-white text-[34px]'>recents</h1>
+              <h1 className='text-secondary-white text-[34px]'>option menu</h1>
+              </div>
+              
+              </div>
       </nav>
     </>
   )
