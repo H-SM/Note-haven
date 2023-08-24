e6e4dc1fb27420f7c8d98f4b6f49851d8132d777
Harman Singh <98532264+H-SM@users.noreply.github.com>
2023-08-24 10:33:29 +0530

    password visibility

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -8,8 +8,12 @@ function Signup(props) {
   const handleSubmit =async (e) => {
         e.preventDefault();
         const host = "http://localhost:5000";
+        if(credentails.password !== credentails.cpassword){
+          alert("Recheck your new password!");
+          return ;
+        }
         const {name, email, password} = credentails;
-        console.log(credentails) ;
+        console.log(credentails);
         const req = await fetch(`${host}/api/auth/createuser`, {
             method: "POST",
             headers: {
@@ -35,6 +39,12 @@ function Signup(props) {
   const handleClick = () => {
     navigate("/login");
 }
+
+const [showPassword, setShowPassword] = useState(false);
+
+const togglePasswordVisibility = () => {
+  setShowPassword(!showPassword);
+};
 
   return (
     <div className="text-white w-screen h-screen relative">
@@ -68,7 +78,27 @@ function Signup(props) {
   </div>
   <div className="mb-3">
     <label htmlFor="password" className="form-label  font-mono text-[18px]">Password</label>
-    <input type="password" className="text-[#ffffff] font-mono leading-[28px] pr-[1rem] pl-[1rem] pt-[0.375rem] pb-[0.375rem] bg-[#684d25c8]/20 border-0 rounded-md ring-1 ring-[#e49012c8]/50 w-full block text-[18px] focus:outline-none focus:shadow-md focus:shadow-orange-400/40 transition ease-in-out duration-300" id="password" name="password" placeholder="your Password..." onChange={onChange} minLength={5} required/>
+    <div className='relative'>
+    <input type={showPassword ? 'text' : 'password'} className="text-[#ffffff] font-mono leading-[28px] pr-[1rem] pl-[1rem] pt-[0.375rem] pb-[0.375rem] bg-[#684d25c8]/20 border-0 rounded-md ring-1 ring-[#e49012c8]/50 w-full block text-[18px] focus:outline-none focus:shadow-md focus:shadow-orange-400/40 transition ease-in-out duration-300" id="password" name="password" placeholder="your Password..." onChange={onChange} minLength={5} required/>
+    <button
+              type="button"
+              className="absolute right-3 top-[0.3vh] transform  text-[#ff8000] focus:outline-none"
+              onClick={togglePasswordVisibility}
+            >
+             
+              {showPassword ? (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+              </svg>              
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                </svg>
+
+              )}
+            </button>
+            </div>
   </div>
   <div className="mb-3">
     <label htmlFor="cpassword" className="form-label  font-mono text-[18px]">Confirm Password</label>
--- src/components/passwordUpdator.js
+++ src/components/passwordUpdator.js
@@ -2,6 +2,16 @@ import React, { useState } from 'react';
 
 const PasswordUpdater = () => {
     const [password, setPassword ]= useState({ oldpassword:"", newpassword:""});
+    const [showOldPassword, setShowOldPassword] = useState(false);
+    const [showNewPassword, setShowNewPassword] = useState(false);
+
+    const toggleOldPasswordVisibility = () => {
+      setShowOldPassword(!showOldPassword);
+    };
+  
+    const toggleNewPasswordVisibility = () => {
+      setShowNewPassword(!showNewPassword);
+    };
 
     const onChangePassword= (e) =>{
         e.preventDefault();
@@ -30,9 +40,10 @@ const PasswordUpdater = () => {
             if(!updatedUser.success){
                 alert(updatedUser.error);
             }
-
+            else{
             console.log('Password updated successfully:', updatedUser);
             alert("Password updated successfully!")
+            }
           } catch (error) {
             console.error('Error updating name:', error);
           }
@@ -56,8 +67,9 @@ const PasswordUpdater = () => {
       <form onSubmit={submithandler}>
         <div className="mb-3">
           <label htmlFor="oldpassword" className="form-label text-[18px] font-normal text-white/50 ">Old Password</label>
+          <div className="relative">
           <input
-            type="password"
+            type={showOldPassword ? 'text' : 'password'}
             className="text-[#ffffff]  pr-[1rem] pl-[1rem] pt-[0.375rem] pb-[0.375rem] bg-[#e49012c8]/10 border-0 rounded-md ring-1 ring-[#e49012c8] w-[400px] block text-[14px] focus:outline-none focus:shadow-md focus:shadow-orange-400 transition ease-in-out duration-300"
             id="oldpassword"
             value={password.oldpassword}
@@ -66,11 +78,30 @@ const PasswordUpdater = () => {
             onKeyDown={onKeyDown}
             placeholder="Your Old Password"
           />
+          <button
+              type="button"
+              className="absolute right-3 top-[0.3vh] transform  text-[#ff8000] focus:outline-none"
+              onClick={toggleOldPasswordVisibility}
+            >
+              {showOldPassword ? (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+              </svg>              
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                </svg>
+
+              )}
+            </button>
+          </div>
         </div>
         <div className="mb-3">
           <label className="form-label  text-[18px] font-normal text-white/50" htmlFor="newpassword">New Password</label>
+          <div className="relative">
           <input
-            type="password"
+            type={showNewPassword ? 'text' : 'password'}
             className="text-[#ffffff]  pr-[1rem] pl-[1rem] pt-[0.375rem] pb-[0.375rem] bg-[#e49012c8]/10 border-0 rounded-md ring-1 ring-[#e49012c8] w-[400px] block text-[14px] focus:outline-none focus:shadow-md focus:shadow-orange-400 transition ease-in-out duration-300"
             id="newpassword"
             name="newpassword"
@@ -79,6 +110,24 @@ const PasswordUpdater = () => {
             onKeyDown={onKeyDown}
             placeholder="Your New Password"
           />
+          <button
+              type="button"
+              className="absolute right-3 top-[0.3vh] transform  text-[#ff8000] focus:outline-none"
+              onClick={toggleNewPasswordVisibility}
+            >
+              {showNewPassword ? (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+              </svg>              
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                </svg>
+
+              )}
+            </button>
+          </div>
         </div>
         <button
           type="button"
